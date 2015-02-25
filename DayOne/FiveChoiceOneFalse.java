18ed11870b9a72fc2b1e0ad94ff33fccb2b9b3e7
Xett575 <Schroderthefirst@gmail.com>
2015-02-24 19:22:59 -0500

    added println to keep up commit streak
    I'm not proud

--- DayOne/FiveChoiceOneFalse.java
+++ DayOne/FiveChoiceOneFalse.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class FiveChoiceOneFalse {
     public void CantHandleTheTruth(){
         
+        System.out.println(" ");
         System.out.println("");
         
     }
