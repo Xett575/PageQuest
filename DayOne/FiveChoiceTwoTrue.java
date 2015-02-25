a9d93cf8d192b2af3266fb1ecbcf32074bf7b1da
Xett575 <Schroderthefirst@gmail.com>
2015-02-24 19:26:26 -0500

    Added options but still need to expand story

--- DayOne/FiveChoiceTwoTrue.java
+++ DayOne/FiveChoiceTwoTrue.java
@@ -10,7 +10,8 @@ public class FiveChoiceTwoTrue {
         System.out.println("You decide not to take any chances and wander over to the opposite side.");
         System.out.println("There's a few people scattered toward the wall each at their own table and some man is sitting at the piano playing a gentle evening tune.");
         System.out.println(" ");
-    
+        System.out.println("1. ");
+        System.out.println("2. ");
     }
     
 }
