dcad37f6c9bdb7a62ea4b437ee14c7a12a2bc4d5
Xett575 <Schroderthefirst@gmail.com>
2015-02-24 19:24:48 -0500

    Added options but still need to expand story

--- DayOne/FiveChoiceTwoFalse.java
+++ DayOne/FiveChoiceTwoFalse.java
@@ -8,7 +8,10 @@ public class FiveChoiceTwoFalse {
     
         System.out.println(" ");
         System.out.println("Your curiosity gets the best of you as you wander closer to the loud men.");
-        System.out.println("Their voices are mixed and it becomes hard to tell if they are yelling at each other or are simple victims of screaming voice syndrome.");        System.out.println(" ");
+        System.out.println("Their voices are mixed and it becomes hard to tell if they are yelling at each other or are simple victims of screaming voice syndrome.");     
+        System.out.println(" ");
+        System.out.println("1. ");
+        System.out.println("2. ");
         
     }
     
