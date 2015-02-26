45f443c6d5f3f2eb4b7d48a5d0b508e94b55f409
Xett575 <Schroderthefirst@gmail.com>
2015-02-26 15:59:53 -0500

    Get a different room

---
+++ DayOne/SevenChoiceThreeFalse.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SevenChoiceThreeFalse {
+ 
+    public void BecomeOutraged(){
+    
+        System.out.println("");
+        System.out.println("You suddenly become outraged at the extra fine.");
+        System.out.println("You incline yourself to educate the hostess about the old world and how the customer is always right.");
+        System.out.println("After finishing your monologue yoiu turn your back to her and think of what to do next.");
+        System.out.println("1. Go back to your room and do something than be here.");
+        System.out.println("2. Try to find something to so down here.");
+    }
+    
+}
---
+++ DayOne/SevenChoiceThreeTrue.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SevenChoiceThreeTrue {
+ 
+    public void GetDifferentRoom(){
+    
+        System.out.println("");
+        System.out.println("You grudgingly hand over the extra farths and she takes back your key.");
+        System.out.println("After the transaction is completed you are left with a silver key that has an attached tag with the number 20 printed on it.");
+        
+        
+    }
+    
+}
